5ef15ffbc264156c67869a1c770eec162d7c490e
BuildTools <unconfigured@null.spigotmc.org>
2025-04-27 20:21:44 +0200

    Partie 1 : Finie Partie 2 : début structure

---
+++ bmp24.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bmp24.h"
+
+// Constantes pour les offsets des champs de l'en-tête BMP
+#define BITMAP_MAGIC 0x00 // offset 0
+#define BITMAP_SIZE 0x02 // offset 2
+#define BITMAP_OFFSET 0x0A // offset 10
+#define BITMAP_WIDTH 0x12 // offset 18
+#define BITMAP_HEIGHT 0x16 // offset 22
+#define BITMAP_DEPTH 0x1C // offset 28
+#define BITMAP_SIZE_RAW 0x22 // offset 34
+// Constante pour le type de fichier BMP
+#define BMP_TYPE 0x4D42 // 'BM' en hexadécimal
+// Constantes pour les tailles des champs de l'en-tête BMP
+#define HEADER_SIZE 0x0E // 14 octets
+#define INFO_SIZE 0x28 // 40 octets
+// Constantes pour les valeurs de profondeur de couleur
+#define DEFAULT_DEPTH 0x18 // 24
+
+/*
+* @brief Positionne le curseur de fichier à la position position dans le fichier file,
+* puis lit n éléments de taille size dans buffer.
+* @param position La position à partir de laquelle il faut lire dans file.
+* @param buffer Le buffer pour stocker les données lues.
+* @param size La taille de chaque élément à lire.
+* @param n Le nombre d'éléments à lire.
+* @param file Le descripteur de fichier dans lequel il faut lire.
+* @return void
+*/
+void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+    fseek(file, position, SEEK_SET);
+    fread(buffer, size, n, file);
+}
+/*
+* @brief Positionne le curseur de fichier à la position position dans le fichier file,
+* puis écrit n éléments de taille size depuis le buffer.
+* @param position La position à partir de laquelle il faut écrire dans file.
+* @param buffer Le buffer contenant les éléments à écrire.
+* @param size La taille de chaque élément à écrire.
+* @param n Le nombre d'éléments à écrire.
+* @param file Le descripteur de fichier dans lequel il faut écrire.
+* @return void
+*/
+void file_rawWrite (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+    fseek(file, position, SEEK_SET);
+    fwrite(buffer, size, n, file);
+}
+
